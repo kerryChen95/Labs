bdfdd11f284e5132e0f3f3bbcc57ca017733d58f
kerry95 <95kerry@gmail.com>
2013-03-27 11:07:59 +0800

    Update example: cross-origin by `img` tag.

--- 201303/cross-origin/server2/routes/index.js
+++ 201303/cross-origin/server2/routes/index.js
@@ -3,8 +3,7 @@ var path = require('path');
 exports['cross-origin'] = function(app){
 	// receive cross-origin request
 	app.get('/img', function (req, res) {
+		console.log('Receive cross-origin image request, query string is: ', req.query);
 	  res.sendfile(path.join(app.get('root path'), 'public/images/kerry95.jpg'));
-	  // res.send(200, 'data respond to img.src request');
-	  // Todo: set response header `Content-Type` to correct data type, to 
 	});
 };
