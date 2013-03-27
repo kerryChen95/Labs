f02c77b2ba46688e7377339bb61fa31c2983290d
kerry95 <95kerry@gmail.com>
2013-03-27 17:47:54 +0800

    Add example: cross-origin by XHR without `Access-Control-Allow-Origin`

--- 201303/cross-origin/server1/public/javascripts/cross_origin.js
+++ 201303/cross-origin/server1/public/javascripts/cross_origin.js
@@ -20,6 +20,43 @@ document.body.onload = function () {
 		document.body.appendChild(p);
 	}
 
+	// Cross-origin by XMLHttpRequest without `Access-Control-Allow-Origin`:
+	// 目标服务器能接收到请求，
+	// 但HTTP响应报文会被浏览器屏蔽，无法在浏览器端通过JS获取，
+	// 并且抛出一个无法在与xhr有关的代码块中捕获的error。
+	(function () {
+		var xhr;
+
+		var append = function () {
+			arguments[0] = 'Example: cross-origin by XHR<hr/>' + arguments[0];
+			return _append.apply( null, arguments );
+		}
+
+		try {
+			xhr = new XMLHttpRequest();
+			xhr.onreadystatechange = function () {
+				append('xhr.readyState = ' + xhr.readyState);
+
+				if (xhr.readyState === xhr.DONE) {
+					append('HTTP status code: ' + xhr.status);
+					if (xhr.status >= 200 && xhr.status <= 299) {
+						append('And get a success status code ' + xhr.status);
+						append('response text: ' + xhr.responseText);
+					} else {
+						// we make target server respond status code 200,
+						// so if received full response but status code is not 200,
+						// we think that XHR has stoped due to an error
+						append('XHR is stopped by an error: if XHR load resource from an origin that is not allowed by `Access-Control-Allow-Origin`, it will be canceled by browser once it receive response headers from a cross-origin server (tested in Chrome 25), in other words, `xhr.readyState` can change to `xhr.DONE` as normal, but it retrieves no response data, and HTTP status code is also set to 0', {color: 'red'});
+					}
+				}
+			};
+			xhr.open('GET', target + 'xhr?name=value');
+			xhr.send();
+		} catch (e) {
+			console.log('Can not catch this error: ', e);
+		}
+	})();
+
 	// Cross-origin by `img` tag:
 	// 目标服务器能接受到请求，但返回的非图片数据会被浏览器屏蔽，
 	// 无法在JS中获取，
--- 201303/cross-origin/server2/routes/index.js
+++ 201303/cross-origin/server2/routes/index.js
@@ -2,6 +2,10 @@ var path = require('path');
 
 exports['cross-origin'] = function(app){
 	// receive cross-origin request
+	app.get('/xhr', function (req, res) {
+		console.log('Receive cross-origin XHR request, query string: ', req.query);
+		res.send(200, 'Received cross-origin XHR request');
+	});
 	app.get('/img', function (req, res) {
 		console.log('Receive cross-origin image request, query string is: ', req.query);
 	  res.sendfile(path.join(app.get('root path'), 'public/images/kerry95.jpg'));
