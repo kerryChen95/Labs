c84a966f8fa89b43144f29290b39ceed0482f372
kerry95 <95kerry@gmail.com>
2013-03-27 17:42:05 +0800

    Update example: cross-origin by `img` tag

--- 201303/cross-origin/server1/public/javascripts/cross_origin.js
+++ 201303/cross-origin/server1/public/javascripts/cross_origin.js
@@ -8,6 +8,18 @@ document.body.onload = function () {
 	console.log('document loaded');
 	var target = 'http://localhost:3001/';
 
+	function _append (content, style) {
+		var p = document.createElement('p'),
+			prop;
+		p.innerHTML = content;
+		for (prop in style) {
+			if (style.hasOwnProperty(prop)) {
+				p.style[prop] = style[prop];
+			}
+		}
+		document.body.appendChild(p);
+	}
+
 	// Cross-origin by `img` tag:
 	// 目标服务器能接受到请求，但返回的非图片数据会被浏览器屏蔽，
 	// 无法在JS中获取，
@@ -17,15 +29,19 @@ document.body.onload = function () {
 	(function () {
 		var img = document.createElement('img'),
 			body = document.body;
+
+		var append = function () {
+			arguments[0] = 'Example: cross-origin by `img.src`<hr/>' + arguments[0];
+			return _append.apply( null, arguments );
+		}
+
+		img.style.display = 'none';
 		body.appendChild(img);
 		img.src = target + 'img?name=value';
+		append('An cross-origin GET request is sent by `img.src`');
 
 		img.onload = function () {
-			var p = document.createElement('p');
-			p.innerHTML = 'img loaded: width ' + img.naturalWidth + 'px, height ' + img.naturalHeight + 'px.';
-			body.appendChild(p);
+			append('Image\'s width is ' + img.naturalWidth + 'px, and height is ' + img.naturalHeight + 'px.');
 		}
 	})();
-
-	// 
-}
+};
